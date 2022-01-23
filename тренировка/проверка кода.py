b039265a69af8c7e92009f36e6ea6a0e99321de4
Anton <lemonydarkwood@gmail.com>
2022-01-23 13:50:37 +0900

    сохранение тренировачых материалов

--- тренировка/проверка кода.py
+++ тренировка/проверка кода.py
@@ -1,3 +1,38 @@
-n = int(input('Введите день: '))
-res = ["понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"]
-print(res[n-1])
+# n = int(input('Введите день: '))
+# res = ["понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"]
+# print(res[n-1])
+
+# e = int(input('введите первое число: '))
+# f = int(input('введите второе число: '))
+# e *= e
+# if(e == f):
+#     print('Первое число является квадратом второго.')
+# else:
+#     print('Перво число не является квадратом второго.')
+
+import random
+
+# number1 = random.randint(1,10)
+# number2 = random.randint(1,10)
+# if(number1 > number2):
+#     print('Число', number1, 'большее')
+#     print('Число', number2, 'меньшее')
+# elif(number1 < number2):
+#     print('Число', number1,'меньшее ')
+#     print('Число', number2,'большее')
+# else:
+#     print('Числа равны.')
+
+# i = random.randint(1, 10)
+# o = random.randint(1, 10)
+# p = random.randint(1, 10)
+
+# max = i
+# if(i > max):
+#     max = i
+# if(o > max):
+#     max = o
+# if(p > max):
+#     max = p
+# print('i =', i, 'o =', o, 'p =', p)
+# print(max)
